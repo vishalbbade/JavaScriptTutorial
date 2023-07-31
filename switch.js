9d94126938efe90e046a52939c8bce027c166c34
Vishal B. Bade <140912894+BadeVishal@users.noreply.github.com>
2023-07-31 05:46:49 +0000

    switch file

---
+++ switch.js
@@ -0,0 +1,21 @@
+// Control Flow
+// Switch Statement
+
+let val = 2;
+switch (val) {
+    case 1:
+        console.log("1");
+        break;
+        
+    case 5:
+        console.log("5");
+        break;
+        
+    case 10:
+        console.log("10");
+        break;
+        
+
+    default:
+        console.log("invalid");
+};
